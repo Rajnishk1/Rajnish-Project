47993218b62276305a9ece1d19267cef2ea73f42
Rajnish <rajnish151293@gmail.com>
2021-10-12 18:32:44 +0530

    First Committ

--- registration-jsp-servlet-jdbc-Employees/src/main/java/com/in/webProject/registrationForm/controller/EmployeeServlet.java
+++ registration-jsp-servlet-jdbc-Employees/src/main/java/com/in/webProject/registrationForm/controller/EmployeeServlet.java
@@ -34,7 +34,8 @@ public class EmployeeServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		//response.sendRedirect("employeeregister.jsp");
 		RequestDispatcher dispatcher =request.getRequestDispatcher("/WEB-INF/views/employeeregister.jsp");
 		dispatcher.forward(request, response);
 	}
